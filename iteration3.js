f681b80f0cf95f9a54c899eee1a7ae2a77dd5b12
Antonio Maestre Vargas <antoniomaestrevargasgmail.com@MBP-de-Antonio.home>
2021-10-01 18:24:10 +0200

    iteration3

---
+++ iteration3.js
@@ -0,0 +1,23 @@
+//1.1
+let multiplicacion=10*5;
+console.log(multiplicacion);
+
+//1.2
+let division=10/2;
+console.log(division);
+
+//1.3
+let resto=15%9;
+console.log(resto);
+
+//1.4
+let y=10;
+let z=5;
+let x=y+z;
+console.log(x);
+
+//1.5
+let y=10;
+let z=5;
+let x=y*z;
+console.log(x);
