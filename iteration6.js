2d2b4dc002e2c427516693bde375cc7c11d2b897
Antonio Maestre Vargas <antoniomaestrevargasgmail.com@MacBook-Pro-de-Antonio.local>
2021-10-05 16:57:19 +0200

    iteration6

---
+++ iteration6.js
@@ -0,0 +1,29 @@
+//1.1
+/*
+let numbe=[1,2,3,4,5,6,7,8,9];
+for(i=0;i<numbe.length;i++){
+ console.log(numbe[i])   
+}
+*/
+
+//1.2
+//1.2
+/*
+let number=[1,2,3,4,5,6,7,8,9];
+for(let i=0;i<=number.length;i++){
+    if(number[i]%2==0){
+        console.log(number[i]);
+    }
+}
+*/
+let number=[1,2,3,4,5,6,7,8,9,10];
+for(let i=0;i<number.length;i++){
+    if(number[i]<=9){
+        console.log('Intentando dormir');
+    }else{
+        console.log('Dormido')
+    }
+}
+
+
+
